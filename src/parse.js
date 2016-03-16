5fc62adf4ee63cbe2dfba8c9638da57ed0f18a5a
Alexander Kosiuk <kosyuk@gmail.com>
2016-03-16 07:23:50 +0200

    Relational operator support

--- src/parse.js
+++ src/parse.js
@@ -37,7 +37,32 @@ var OPERATORS = {
 	},
 	'%': function(self, locals, a, b) {
 		return a(self, locals) % b(self, locals);
-	}
+	},
+	'<': function(self, locals, a, b) {
+		return a(self, locals) < b(self, locals);
+	},
+	'>': function(self, locals, a, b) {
+		return a(self, locals) > b(self, locals);
+	},
+	'<=': function(self, locals, a, b) {
+		return a(self, locals) <= b(self, locals);
+	},
+	'>=': function(self, locals, a, b) {
+		return a(self, locals) >= b(self, locals);
+	},
+	'==': function(self, locals, a, b) {
+		return a(self, locals) == b(self, locals);
+	},
+	'!=': function(self, locals, a, b) {
+		return a(self, locals) != b(self, locals);
+	},
+	'===': function(self, locals, a, b) {
+		return a(self, locals) === b(self, locals);
+	},
+	'!==': function(self, locals, a, b) {
+		return a(self, locals) !== b(self, locals);
+	},
+	'=': _.noop
 };
 
 var CALL = Function.prototype.call;
@@ -179,7 +204,7 @@ Lexer.prototype.lex = function(text) {
 			this.readNumber();
 		} else if (this.is('\'"')) {
 			this.readString(this.ch);
-		} else if (this.is('[],{}:.()=')) {
+		} else if (this.is('[],{}:.()')) {
 			this.tokens.push({
 				text: this.ch
 			});
@@ -189,8 +214,26 @@ Lexer.prototype.lex = function(text) {
 		} else if (this.isWhitespace(this.ch)) {
 			this.index++;
 		} else {
+			var ch2 = this.ch + this.peek();
+			var ch3 = this.ch + this.peek() + this.peek(2);
+
 			var fn = OPERATORS[this.ch];
-			if (fn) {
+			var fn2 = OPERATORS[ch2];
+			var fn3 = OPERATORS[ch3];
+
+			if (fn3) {
+				this.tokens.push({
+					text: ch3,
+					fn: fn3
+				});
+				this.index += 3;
+			} else if (fn2) {
+				this.tokens.push({
+					text: ch2,
+					fn: fn2
+				});
+				this.index += 2;
+			} else if (fn) {
 				this.tokens.push({
 					text: this.ch,
 					fn: fn
@@ -341,9 +384,10 @@ Lexer.prototype.readIdent = function() {
 	}
 };
 
-Lexer.prototype.peek = function() {
-	return this.index < this.text.length - 1 ?
-		this.text.charAt(this.index + 1) :
+Lexer.prototype.peek = function(n) {
+	n = n || 1;
+	return this.index + n < this.text.length ?
+		this.text.charAt(this.index + n) :
 		false;
 };
 
@@ -517,12 +561,12 @@ Parser.prototype.functionCall = function(fnFn, contextFn) {
 };
 
 Parser.prototype.assignment = function() {
-	var left = this.additive();
+	var left = this.equality();
 	if (this.expect('=')) {
 		if (!left.assign) {
 			throw 'Implies assignment but cannot be assigned to';
 		}
-		var right = this.additive();
+		var right = this.equality();
 		return function(scope, locals) {
 			return left.assign(scope, right(scope, locals), locals);
 		};
@@ -573,6 +617,24 @@ Parser.prototype.binaryFn = function(left, op, right) {
 	};
 	fn.constant = left.constant && right.constant;
 	return fn;
+};
+
+Parser.prototype.equality = function() {
+	var left = this.relational();
+	var operator;
+	while ((operator = this.expect('==', '!=', '===', '!=='))) {
+		left = this.binaryFn(left, operator.fn, this.relational());
+	}
+	return left;
+};
+
+Parser.prototype.relational = function() {
+	var left = this.additive();
+	var operator;
+	while ((operator = this.expect('<', '>', '<=', '>='))) {
+		left = this.binaryFn(left, operator.fn, this.additive());
+	}
+	return left;
 };
 
 Parser.ZERO = _.extend(_.constant(0), {constant: true});

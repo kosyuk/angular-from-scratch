505889bfc6a7489752df83be4110f0c82d0d4428
Alexander Kosiuk <kosyuk@gmail.com>
2016-03-20 19:38:14 +0200

    modules, register modules

---
+++ src/loader.js
@@ -0,0 +1,24 @@
+/* jshint globalstrict: true */
+'use strict';
+
+function setupModuleLoader(window) {
+	var ensure = function(obj, name, factory) {
+		return obj[name] || (obj[name] = factory());
+	};
+
+	var angular = ensure(window, 'angular', Object);
+
+	var createModule = function(name, requires){
+		var moduleInstance = {
+			name: name,
+			requires: requires
+		};
+		return moduleInstance;
+	};
+
+	ensure(angular, 'module', function(){
+		return function(name, requires) {
+			return createModule(name, requires);
+		};
+	});
+}
